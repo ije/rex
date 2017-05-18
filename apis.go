95f791551ce32c8e9c2d88c938988a92b2974b54
Je <git@iamje.com>
2017-05-18 16:42:45 +0800

    panic when register api with an invalid handle

--- apis.go
+++ apis.go
@@ -15,30 +15,32 @@ type apiHandler struct {
 
 type APIService map[string]map[string]apiHandler
 
-func (s APIService) Get(endpoint string, handler interface{}, privileges user.Privileges) {
-	s.register("GET", endpoint, handler, privileges)
+func (s APIService) Get(endpoint string, handle interface{}, privileges user.Privileges) {
+	s.register("GET", endpoint, handle, privileges)
 }
 
-func (s APIService) Post(endpoint string, handler interface{}, privileges user.Privileges) {
-	s.register("POST", endpoint, handler, privileges)
+func (s APIService) Post(endpoint string, handle interface{}, privileges user.Privileges) {
+	s.register("POST", endpoint, handle, privileges)
 }
 
-func (s APIService) Put(endpoint string, handler interface{}, privileges user.Privileges) {
-	s.register("PUT", endpoint, handler, privileges)
+func (s APIService) Put(endpoint string, handle interface{}, privileges user.Privileges) {
+	s.register("PUT", endpoint, handle, privileges)
 }
 
-func (s APIService) Delete(endpoint string, handler interface{}, privileges user.Privileges) {
-	s.register("DELETE", endpoint, handler, privileges)
+func (s APIService) Delete(endpoint string, handle interface{}, privileges user.Privileges) {
+	s.register("DELETE", endpoint, handle, privileges)
 }
 
-func (s APIService) register(method string, endpoint string, handler interface{}, privileges user.Privileges) {
-	switch v := handler.(type) {
+func (s APIService) register(method string, endpoint string, handle interface{}, privileges user.Privileges) {
+	switch v := handle.(type) {
 	case func(), func(*Context), func(*XService), func(*Context, *XService), func(*XService, *Context):
 		if s[method] == nil {
 			s[method] = map[string]apiHandler{}
 		}
 		endpoint = strings.Trim(endpoint, "/")
 		s[method][endpoint] = apiHandler{privileges, v}
+	default:
+		panic("register %s /api/%s: the handle should be a valid api handle\n available api handle types:\n\tfunc()\n\tfunc(*webx.Context)\n\tfunc(*webx.XService)`, 1func(*webx.Context, *webx.XService)\n\tfunc(*webx.XService, *webx.Context)")
 	}
 }
 
