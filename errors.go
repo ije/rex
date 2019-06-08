89080552ecf0112d9a9d8b4daec0eebc53f5af5f
Je <git@iamje.com>
2019-06-09 00:36:40 +0800

    feat: add error types

--- errors.go
+++ errors.go
@@ -7,3 +7,22 @@ type contextPanicError struct {
 func (err *contextPanicError) Error() string {
 	return err.msg
 }
+
+type InvalidError struct {
+	Code    int
+	Message string
+}
+
+func Invalid(code int, message string) *InvalidError {
+	return &InvalidError{code, message}
+}
+
+func (err *InvalidError) Error() string {
+	return err.Message
+}
+
+type ServerError string
+
+func (err ServerError) Error() string {
+	return string(err)
+}
