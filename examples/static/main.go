cf1da44feae85367a63608e518126532c1ef349f
Je <git@iamje.com>
2020-04-20 16:06:03 +0800

    chore: update examples

--- examples/static/main.go
+++ examples/static/main.go
@@ -4,8 +4,17 @@ import (
 	"github.com/ije/rex"
 )
 
+const indexHTML = `
+<h1>Welcome to use REX!</h1>
+<p><a href="/www">WWW</a></p>
+`
+
 func main() {
-	rex.Get("/*", rex.Static("./www", "e404.html"))
+	rex.Get("/", func(ctx *rex.Context) {
+		ctx.HTML(indexHTML)
+	})
+
+	rex.Static("/www/", "./www", "e404.html")
 
 	rex.Start(8080)
 }
