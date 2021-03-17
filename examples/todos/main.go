67d167b719f94474073c2c10793e6ae0e07014f2
X <i@jex.me>
2021-03-17 18:59:12 +0800

    chore: update todos example

--- examples/todos/main.go
+++ examples/todos/main.go
@@ -8,30 +8,27 @@ const indexHTML = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>TODOS</title>
+    <title>Todos by REX</title>
 </head>
 <body>
     <h1>TODOS</h1>
-
     {{if .user}}
         <form method="post" action="/logout"> 
-            <p>Welcome back, <strong>{{.user}}</strong>!</p>
-            <p><input value="Logout" type="submit"></p>
+            <p>Welcome back, <strong>{{.user}}</strong>! <input value="Logout" type="submit"></p>
         </form>
         <h2>Todos List:</h2>
         <ul>
             {{range $index,$todo := .todos}}
             <li>
                 <form style="display:inline-block;" method="post" action="/delete-todo">
-                    {{$todo}} &nbsp; <input name="index" type="hidden" value="{{$index}}"> <input value="X" type="submit">
+                    {{$todo}} &nbsp; <input name="index" type="hidden" value="{{$index}}"> <input value="x" type="submit">
                 </form>
             </li>
             {{end}}
         </ul>
-        <form method="post" action="/add-todo">
-            <input name="todo" type="text">
-            <input value="Add" type="submit">
-        </form>
+		<form method="post" action="/add-todo">
+			<input name="todo" type="text" placeholder="Add">
+		</form>
     {{else}}
         <form method="post" action="/login">
             <input name="user" type="text">
@@ -52,13 +49,18 @@ func (u *user) Permissions() []string {
 }
 
 func main() {
-	todos := map[string][]string{}
+	todos := []string{}
 
 	rex.Use(func(ctx *rex.Context) interface{} {
 		if ctx.Session().Has("USER") {
+			name := string(ctx.Session().Get("USER"))
+			permissions := []string{}
+			if name == "admin" {
+				permissions = []string{"add", "remove"}
+			}
 			ctx.SetACLUser(&user{
-				name:        string(ctx.Session().Get("USER")),
-				permissions: []string{"add", "remove"},
+				name:        name,
+				permissions: permissions,
 			})
 		}
 		return nil
@@ -69,37 +71,35 @@ func main() {
 		aclUser := ctx.ACLUser()
 		if aclUser != nil {
 			data["user"] = aclUser.(*user).name
-			data["todos"] = todos[aclUser.(*user).name]
+			data["todos"] = todos
 		}
 		return rex.HTML(indexHTML, data)
 	})
 
 	rex.Mutation("add-todo", rex.ACL("add"), func(ctx *rex.Context) interface{} {
 		todo := ctx.Form.Require("todo")
-		user := ctx.ACLUser().(*user).name
-		todos[user] = append(todos[user], todo)
+		todos = append(todos, todo)
 		return rex.Redirect("/", 301)
 	})
 
 	rex.Mutation("delete-todo", rex.ACL("remove"), func(ctx *rex.Context) interface{} {
 		index := ctx.Form.RequireInt("index")
-		user := ctx.ACLUser().(*user).name
-		userTodos := todos[user]
 		var newTodos []string
-		for i, todo := range userTodos {
+		for i, todo := range todos {
 			if i != int(index) {
 				newTodos = append(newTodos, todo)
 			}
 		}
-		todos[user] = newTodos
+		todos = newTodos
 		return rex.Redirect("/", 301)
 	})
 
 	rex.Mutation("login", func(ctx *rex.Context) interface{} {
 		user := ctx.Form.Value("user")
-		if user != "" {
-			ctx.Session().Set("USER", []byte(user))
+		if user != "admin" && user != "guest" {
+			return rex.Status(403, "invalid user")
 		}
+		ctx.Session().Set("USER", []byte(user))
 		return rex.Redirect("/", 301)
 	})
 
